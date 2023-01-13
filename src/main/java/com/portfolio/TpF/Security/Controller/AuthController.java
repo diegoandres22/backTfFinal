d22b8df1388e9da29a21de7ef150a023bd287af3
diegoandres22 <89799715+diegoandres22@users.noreply.github.com>
2023-01-13 19:37:26 -0300

    Update AuthController.java

--- src/main/java/com/portfolio/TpF/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/TpF/Security/Controller/AuthController.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliodiegovelasquez.web.app")
 
 public class AuthController {
 

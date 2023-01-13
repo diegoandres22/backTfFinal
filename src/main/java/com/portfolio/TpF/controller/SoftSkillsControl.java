b400120d452a53d7f479a703a0eaab6dda37a718
diegoandres22 <89799715+diegoandres22@users.noreply.github.com>
2023-01-13 19:39:18 -0300

    Update SoftSkillsControl.java

--- src/main/java/com/portfolio/TpF/controller/SoftSkillsControl.java
+++ src/main/java/com/portfolio/TpF/controller/SoftSkillsControl.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("soft")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliodiegovelasquez.web.app")
 public class SoftSkillsControl {
 
     @Autowired

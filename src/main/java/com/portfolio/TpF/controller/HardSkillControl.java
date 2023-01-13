bfc47e3efc21159de96b9495ffee10897023edb5
diegoandres22 <89799715+diegoandres22@users.noreply.github.com>
2023-01-13 19:38:47 -0300

    Update HardSkillControl.java

--- src/main/java/com/portfolio/TpF/controller/HardSkillControl.java
+++ src/main/java/com/portfolio/TpF/controller/HardSkillControl.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("hard")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliodiegovelasquez.web.app")
 public class HardSkillControl {
     @Autowired
     private iHaskillsService estuhard;

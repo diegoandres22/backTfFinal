f87dedf287b1b808c553ca0fd48887be13a0f8f8
diegoandres22 <89799715+diegoandres22@users.noreply.github.com>
2023-01-14 17:22:26 -0300

    Update EstudiosControl.java

--- src/main/java/com/portfolio/TpF/controller/EstudiosControl.java
+++ src/main/java/com/portfolio/TpF/controller/EstudiosControl.java
@@ -23,8 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("estudy")
 @CrossOrigin(origins = "https://portfoliodiegovelasquez.web.app")
-
-@CrossOrigin(origins = "https://portfoliodiegovelasquez.firebaseapp.com")
 public class EstudiosControl {
 
     @Autowired

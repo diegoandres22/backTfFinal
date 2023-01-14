471be49fb9af0f99a487b03917f248c3cbde5859
diegoandres22 <89799715+diegoandres22@users.noreply.github.com>
2023-01-14 15:32:05 -0300

    Update ExperienciasLaborales.java

--- src/main/java/com/portfolio/TpF/controller/ExperienciasLaborales.java
+++ src/main/java/com/portfolio/TpF/controller/ExperienciasLaborales.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("exlab")
 @CrossOrigin(origins = "https://portfoliodiegovelasquez.web.app")
+@CrossOrigin(origins = "https://portfoliodiegovelasquez.firebaseapp.com")
 public class ExperienciasLaborales {
 
     @Autowired
